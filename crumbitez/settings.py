4d966a314445584c5135ff0987047f3ef384fa63
Syifa Ananda <syifaananda@Syifas-MacBook-Pro.local>
2024-09-17 20:40:25 +0700

    update

--- crumbitez/settings.py
+++ crumbitez/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://syifa-ananda31-crumbitez.pbp.cs.ui.ac.id", "https://syifa-ananda31-crumbitez.pbp.cs.ui.ac.id"]
